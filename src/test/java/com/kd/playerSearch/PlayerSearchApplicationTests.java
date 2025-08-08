1feacd9ae9f425eac680f79b181d234eb3ba5783
Kalana_KD <kalanakd471@gmail.com>
2025-08-08 12:15:58 +0530

    Create PlayerSearch

---
+++ src/test/java/com/kd/playerSearch/PlayerSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kd.playerSearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayerSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
